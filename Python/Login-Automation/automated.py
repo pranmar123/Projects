7b7602c4d6f2b6a5cd58e41c8535e04d366d9e20
Pranay Marella <34179310+pranmar123@users.noreply.github.com>
2018-06-03 16:36:43 -0600

    Update automated.py

--- Python/Login-Automation/automated.py
+++ Python/Login-Automation/automated.py
@@ -9,23 +9,10 @@ import sys
 
 #This snippet is needed so that when the executable is created,
 #the exe can find the CHROMEDRIVER in its folders.
-def find_data_file(filename):
-    if getattr(sys, 'frozen', False):
-        # The application is frozen
-        datadir = os.path.dirname(sys.executable)
-    else:
-        # The application is not frozen
-        # Change this bit to match where you store your data files:
-        datadir = os.path.dirname(__file__)
-
-    return os.path.join(datadir, filename)
-
-
 
 username = ''
 password = ''
 
-chromedriverpath = find_data_file("chromedriver.exe")
 
 
 #url of the twitter
@@ -35,7 +22,7 @@ url = 'https://www.twitter.com/login/'
 You can download the webdriver at:
 https://chromedriver.storage.googleapis.com/index.html?path=2.38/'''
 
-driver = webdriver.Chrome(chromedriverpath) #This should be the path of where your chromedriver exists
+driver = webdriver.Chrome() #This should be the path of where your chromedriver exists
 
 
 
